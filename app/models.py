ac941dcdaa0b0e396db244d9d2169756d7bd7eb4
Victor Gomes <victorngomes@gmail.com>
2023-11-15 22:06:29 -0300

    models updated, new attributes added to class Student

--- app/models.py
+++ app/models.py
@@ -5,8 +5,11 @@ from .extensions import db  # importando o banco de dados
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
+    # students
 
 
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
+    course_id = db.Column(db.ForeignKey("course.id"))
+
